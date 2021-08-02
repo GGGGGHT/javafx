167f11068d7a82a0cf8f63657aaf992ea5692d24
GGGGGHT <wz9712203617@gmail.com>
2021-08-02 18:20:00 +0800

    feat: window layout

--- src/main/java/com/ggggght/javafx/alertBox/MyApplication.java
+++ src/main/java/com/ggggght/javafx/alertBox/MyApplication.java
@@ -3,27 +3,41 @@ package com.ggggght.javafx.alertBox;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 
 public class MyApplication extends Application {
+	Stage window;
 	@Override
 	public void start(Stage stage) throws IOException {
-		StackPane layout = new StackPane();
-		Button button = new Button("This is a Button");
-		button.setOnAction(e -> closeWindow(stage));
-		layout.getChildren().add(button);
-		Scene scene = new Scene(layout, 320, 240);
-		stage.setTitle("Hello!");
-		stage.setScene(scene);
-		stage.show();
-		stage.setOnCloseRequest(e -> {
-			e.consume();
-			closeWindow(stage);
-		});
+		window = stage;
+		window.setTitle("This is a title");
+
+		HBox topMenu = new HBox();
+		Button buttonA = new Button("File");
+		Button buttonB = new Button("Edit");
+		Button buttonC = new Button("View");
+		topMenu.getChildren().addAll(buttonA, buttonB, buttonC);
+
+		VBox leftMenu = new VBox();
+		Button buttonD = new Button("Project");
+		Button buttonE = new Button("Commit");
+		Button buttonF = new Button("pull Requests");
+		leftMenu.getChildren().addAll(buttonD, buttonE, buttonF);
+
+		BorderPane borderPane = new BorderPane();
+		borderPane.setLeft(leftMenu);
+		borderPane.setTop(topMenu);
+
+		Scene scene = new Scene(borderPane, 300, 250);
+		window.setScene(scene);
+		window.show();
 	}
 
 	public static void main(String[] args) {
