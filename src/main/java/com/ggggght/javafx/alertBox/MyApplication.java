213354ffc172dd057b338f13f2694249d54dbade
GGGGGHT <wz9712203617@gmail.com>
2021-08-02 17:08:32 +0800

    feat: update module-info

---
+++ src/main/java/com/ggggght/javafx/alertBox/MyApplication.java
@@ -0,0 +1,20 @@
+package com.ggggght.javafx.alertBox;
+
+import com.ggggght.javafx.HelloApplication;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class MyApplication extends Application {
+	@Override
+	public void start(Stage stage) throws IOException {
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+}
