9829ca23ebafba4f45ca48a844366465b7fbd14d
GGGGGHT <wz9712203617@gmail.com>
2021-08-03 08:35:55 +0800

    feat: listen choice box selection

--- src/main/java/com/ggggght/javafx/layout/ChoiceApplication.java
+++ src/main/java/com/ggggght/javafx/layout/ChoiceApplication.java
@@ -12,35 +12,40 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ChoiceApplication extends Application {
-	Stage window;
-	@Override
-	public void start(Stage stage) throws IOException {
-		window = stage;
-		window.setTitle("This is a title");
+  Stage window;
 
-		ChoiceBox<String> choiceBox = new ChoiceBox<>();
-		choiceBox.getItems().addAll(List.of("Apples", "Bananas", "Bacon", "Ham"));
-		// set default value it must exist
-		choiceBox.setValue("Apples");
+  @Override
+  public void start(Stage stage) throws IOException {
+    window = stage;
+    window.setTitle("This is a title");
 
-		Button button = new Button("Order Now!");
-		button.setOnAction(e -> handleChoice(choiceBox, choiceBox.getValue()));
-		VBox layout = new VBox(10);
-		layout.setPadding(new Insets(20,20,20,20));
-		layout.getChildren().addAll(choiceBox,button);
+    ChoiceBox<String> choiceBox = new ChoiceBox<>();
+    choiceBox.getItems().addAll(List.of("Apples", "Bananas", "Bacon", "Ham"));
+    // set default value it must exist
+    choiceBox.setValue("Apples");
+    // listen for selection changes
+    choiceBox.getSelectionModel().selectedItemProperty().addListener((a, oValue, nValue) -> {
+      System.out.println(nValue);
+    });
 
-		Scene scene = new Scene(layout, 300, 250);
-		window.setScene(scene);
-		window.show();
-	}
+    Button button = new Button("Order Now!");
+    button.setOnAction(e -> handleChoice(choiceBox, choiceBox.getValue()));
+    VBox layout = new VBox(10);
+    layout.setPadding(new Insets(20, 20, 20, 20));
+    layout.getChildren().addAll(choiceBox, button);
 
-	private void handleChoice(ChoiceBox<String> choiceBox, String value) {
-		choiceBox.getItems().forEach(a -> System.out.printf("%s\t", a));
+    Scene scene = new Scene(layout, 300, 250);
+    window.setScene(scene);
+    window.show();
+  }
 
-		System.out.println("value = " + value);
-	}
+  private void handleChoice(ChoiceBox<String> choiceBox, String value) {
+    choiceBox.getItems().forEach(a -> System.out.printf("%s\t", a));
 
-	public static void main(String[] args) {
-		launch();
-	}
+    System.out.println("value = " + value);
+  }
+
+  public static void main(String[] args) {
+    launch();
+  }
 }
